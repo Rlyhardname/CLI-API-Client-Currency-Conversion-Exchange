2c5dd70e03082d4ba7688f462ee03dc16c61130e
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-06-08 15:27:26 +0300

    -add REQUEST_ERROR message in ValidationMessages.java when API requests go wrong.

--- src/main/java/com/appolica/assessment/messages/ValidationMessages.java
+++ src/main/java/com/appolica/assessment/messages/ValidationMessages.java
@@ -4,4 +4,5 @@ public class ValidationMessages {
     public static final String INVALID_AMOUNT = "Please enter a valid amount";
     public static final String INVALID_CURRENCY = "Please enter a valid currency code";
     public static final String TOO_MANY_ARGUMENTS = "You have exceeded the number of arguments, restart the application and try again!";
+    public static final String REQUEST_ERROR = "Something went wrong with your request... please start over...";
 }
