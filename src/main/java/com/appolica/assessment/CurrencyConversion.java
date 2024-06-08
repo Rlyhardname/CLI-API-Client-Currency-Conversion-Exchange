54aef4bc01fbcf1c5e07992d6bb0fe455cb01761
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-06-08 15:05:21 +0300

    -add application entry point CurrencyConversion.java

---
+++ src/main/java/com/appolica/assessment/CurrencyConversion.java
@@ -0,0 +1,10 @@
+package com.appolica.assessment;
+
+import com.appolica.assessment.apphandler.ConsoleCommandHandler;
+
+public class CurrencyConversion {
+    public static void main(String[] args) {
+        ConsoleCommandHandler.start(args);
+    }
+
+}
