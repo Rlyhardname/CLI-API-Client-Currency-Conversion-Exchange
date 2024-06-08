2898a1400b4cc0ab1be8c3850863756d87958449
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-06-08 15:16:29 +0300

    -add messages package -add ValidationMessages.java with static field messages, used for error handling.

---
+++ src/main/java/com/appolica/assessment/messages/ValidationMessages.java
@@ -0,0 +1,7 @@
+package com.appolica.assessment.messages;
+
+public class ValidationMessages {
+    public static final String INVALID_AMOUNT = "Please enter a valid amount";
+    public static final String INVALID_CURRENCY = "Please enter a valid currency code";
+    public static final String TOO_MANY_ARGUMENTS = "You have exceeded the number of arguments, restart the application and try again!";
+}
