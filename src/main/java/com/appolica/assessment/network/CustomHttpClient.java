27afbb740708108221e510d6b19b9a6197c411d0
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-06-08 15:29:41 +0300

    -add network package -add CustomHttpClient.java wrapper class to encapsulate the needs of our CLI client for sending requests to the fastforex API -add method sendRequest(uri) for sending a request to the api and returning a response<String>

---
+++ src/main/java/com/appolica/assessment/network/CustomHttpClient.java
@@ -0,0 +1,36 @@
+package com.appolica.assessment.network;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static com.appolica.assessment.messages.ValidationMessages.REQUEST_ERROR;
+
+public class CustomHttpClient {
+    public HttpResponse<String> sendRequest(String url) throws NullPointerException,IllegalArgumentException  {
+        HttpRequest request = buildRequest(url);
+        return getResponse(request);
+    }
+
+    private HttpRequest buildRequest(String uri) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .header("accept", "application/json")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        return request;
+    }
+
+    private HttpResponse<String> getResponse(HttpRequest request) {
+        try {
+            return HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            System.err.println(REQUEST_ERROR);
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
