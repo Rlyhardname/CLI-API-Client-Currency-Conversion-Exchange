e9843b8226252fe1febb19fe2108258794a9ac11
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-06-08 15:10:27 +0300

    -add configurations package -add CustomURL.java static configurations file used for building URL requests with final fields:  -domain,convert,historical,query_strings,and; non-final:  -API_KEY
    -add Paths.java for configuration paths to local resources
     -API_KEY_PATH

---
+++ src/main/java/com/appolica/assessment/config/CustomURL.java
@@ -0,0 +1,11 @@
+package com.appolica.assessment.config;
+
+public class CustomURL {
+    public static final String DOMAIN = "https://api.fastforex.io/";
+    public static final String CONVERT = "convert";
+    public static final String HISTORICAL = "historical";
+    public static final String QUERY_STRINGS = "?";
+    public static final String AND = "&";
+    public static String API_KEY;
+
+}
---
+++ src/main/java/com/appolica/assessment/config/Paths.java
@@ -0,0 +1,5 @@
+package com.appolica.assessment.config;
+
+public class Paths {
+    public static final String API_KEY_PATH = "src//main//resources//config.json";
+}
