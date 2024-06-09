1bd41276f7c0b7e3821268ad598bbbb7753317d9
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-06-09 14:41:31 +0300

    -repurpose java POJO for compatability with objectMapper serialization and deserialization, and object creation.

--- src/main/java/com/appolica/assessment/models/ConversionContainer.java
+++ src/main/java/com/appolica/assessment/models/ConversionContainer.java
@@ -1,13 +1,25 @@
 package com.appolica.assessment.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 
-public class ConversionContainer {
-
+public class ConversionContainer implements Container {
+    @JsonProperty("date")
     private LocalDate localDate;
-    private String baseCurrency;
-    private String targetCurrency;
+    @JsonProperty("amount")
     private double amount;
+    @JsonProperty("base_currency")
+    private String baseCurrency;
+    @JsonProperty("target_currency")
+    private String targetCurrency;
+    @JsonProperty("converted_amount")
+    private double convertedAmount;
+
+    public ConversionContainer() {
+    }
 
     public ConversionContainer(LocalDate localDate) {
         this.localDate = localDate;
@@ -42,6 +54,30 @@ public class ConversionContainer {
     }
 
     public void setAmount(double amount) {
-        this.amount = amount;
+        this.amount = bankersRoundingToSecondFractional(amount);
+    }
+
+    public double getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public void setConvertedAmount(double convertedAmount) {
+        this.convertedAmount = bankersRoundingToSecondFractional(convertedAmount);
+    }
+
+    private double bankersRoundingToSecondFractional(double amount) {
+        BigDecimal bd = new BigDecimal(amount).setScale(2, RoundingMode.HALF_EVEN);
+        return bd.doubleValue();
+    }
+
+    @Override
+    public String toString() {
+        return "ConversionContainer{" +
+                "localDate=" + localDate +
+                ", baseCurrency='" + baseCurrency + '\'' +
+                ", targetCurrency='" + targetCurrency + '\'' +
+                ", amount=" + amount +
+                ", convertedAmount=" + convertedAmount +
+                '}';
     }
 }
